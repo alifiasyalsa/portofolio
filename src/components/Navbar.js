500bd8188efc658e1e23d6a5f04441a7ce28ad62
Alifia Syalsabila <alifiasyalsabila96@gmail.com>
2023-04-28 09:25:38 +0700

    Adjust phone number on CV

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -105,7 +105,7 @@ const Navbar = () => {
             ))}
           </ul>
           <button type="button" className={`${containerClass.button} ml-10`}>
-            <a href="CV Alifia Syalsabila - Frontend Developer.pdf" target="_blank">
+            <a href="CV_Alifia Syalsabila - Frontend Developer.pdf" target="_blank">
               Resume
             </a>
           </button>
